71824dad038ea6478beb08f74131d2281d2e16ba
Emma Thorpe <daniel@danielthorpe.me>
2025-03-16 15:51:41 +0000

    feat (swaywm.nix): move startup to nix
    cleans up zshrc from redundant exports

--- swaywm.nix
+++ swaywm.nix
@@ -11,11 +11,21 @@ in
 		programs.sway = {
 			enable = true;
 			wrapperFeatures.gtk = true;
+			extraSessionCommands = ''
+				# QT
+        			export QT_QPA_PLATFORM="wayland;xcb"
+        			export QT_QPA_PLATFORMTHEME=qt5ct
+				# SDL
+        			export SDL_VIDEODRIVER=wayland
+				# Java
+        			export _JAVA_AWT_WM_NONREPARENTING=1
+				# Misc
+        			export CLUTTER_BACKEND=wayland
+        			export WINIT_UNIX_BACKEND=x11
+        			export MOZ_ENABLE_WAYLAND=1
+			'';
+			extraPackages = with pkgs; [ brightnessctl foot grim swayidle swaylock i3status-rust sway-launcher-desktop ];
 		};
-		environment.systemPackages = with pkgs; [
-			i3status-rust
-			sway-launcher-desktop
-		];
 		fonts.packages = with pkgs; [
 			noto-fonts
 			noto-fonts-emoji
