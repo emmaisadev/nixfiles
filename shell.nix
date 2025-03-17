c28d5f2b8bb6554c9a6b9990420f080af654b0b5
Emma Thorpe <daniel@danielthorpe.me>
2025-03-17 20:50:51 +0000

    feat(shell.nix): move tmux configuration to nix

--- shell.nix
+++ shell.nix
@@ -11,4 +11,21 @@
 		syntaxHighlighting.enable = true;
 		autosuggestions.enable = true;
 	};
+	programs.tmux = {
+		enable = true;
+		reverseSplit = true;
+		terminal = "tmux-direct";
+		newSession = true;
+		keyMode = "vi";
+		historyLimit = 50000;
+		extraConfig = ''
+			# Enable mouse support
+			set -g mouse on
+			# Set pane navigation
+			bind -n M-Left select-pane -L
+			bind -n M-Right select-pane -R
+			bind -n M-Up select-pane -U
+			bind -n M-Down select-pane -D
+		'';
+	};
 }
