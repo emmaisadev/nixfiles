56aa234efe510cfecd360ba6e01856d1107ef8a0
Emma Thorpe <iam@emmathe.dev>
2025-05-21 19:47:51 +0100

    Add Nix files

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+	nixpkgs.url = "nixpkgs/nixos-unstable";
+	home-manager.url = "github:nix-community/home-manager";
+	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ { self, nixpkgs, home-manager, ... }:
+    let system = "aarch64-linux"; in {
+      nixosConfigurations = {
+        emmathorpe-mbp = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./system/machine/MBP-Asahi/configuration.nix
+	    ./emmathorpe/user.nix
+	    ./emmathorpe/swaymw.nix
+	    home-manager.nixosModules.home-manager
+	    {
+		home-manager.useGlobalPkgs = true;
+		home-manager.useUserPackages = true;
+		home-manager.users.emmathorpe = import ./emmathorpe/home.nix;
+	    }
+          ];
+        };
+      };
+    };
+}
