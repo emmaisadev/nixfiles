da4f05d36095324e60e46664052e08293597a882
Emma Thorpe <daniel@danielthorpe.me>
2025-05-29 16:36:13 +0100

    feat: get X1 Carbon working

--- flake.nix
+++ flake.nix
@@ -7,12 +7,10 @@
 	home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, ... }:
-    let system = "aarch64-linux"; in {
-      nixosConfigurations = {
-        emmathorpe-mbp = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit inputs; };
+  outputs = inputs @ { self, nixpkgs, home-manager, ... }: {
+      nixosConfigurations.emmathorpe-mbp = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";  
+	specialArgs = { inherit inputs; };
           modules = [
             ./system/machine/MBP-Asahi/configuration.nix
 	    ./emmathorpe/user.nix
@@ -25,6 +23,20 @@
 	    }
           ];
         };
-      };
-    };
+        nixosConfigurations.emmathorpe-x1c = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";  
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./system/machine/X1/configuration.nix
+	    ./emmathorpe/user.nix
+	    ./emmathorpe/swaywm.nix
+	    home-manager.nixosModules.home-manager
+	    {
+		home-manager.useGlobalPkgs = true;
+		home-manager.useUserPackages = true;
+		home-manager.users.emmathorpe = import ./emmathorpe/home.nix;
+	    }
+          ];
+        };
+     };
 }
