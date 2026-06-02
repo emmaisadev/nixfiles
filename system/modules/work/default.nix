d6a962f093c88c668476ab6b2c0006c2f48ebf4e
Emma Thorpe <emma.thorpe@citrix.com>
2026-06-02 12:46:07 +0000

    feat: flesh out work module and pin claude-code to nixpkgs unstable
    Migrate git config to the settings option, fix the signing key path and email, add argo-rollouts/google-cloud-sdk and other tooling, enable go, and wire a dedicated nixpkgs-unstable input so claude-code tracks unstable independently of the pinned nixpkgs.

--- system/modules/work/default.nix
+++ system/modules/work/default.nix
@@ -1,20 +1,54 @@
 { config, pkgs, inputs, lib, ... }:
 
+let
+  unstable = import inputs.nixpkgs-unstable {
+    inherit (pkgs.stdenv.hostPlatform) system;
+    config.allowUnfree = true;
+  };
+in
 {
   programs.git = {
-    userEmail = "emma.thorpe@cloud.com";
+    settings = {
+      commit.gpgsign = true;
+      tag.gpgsign = true;
+      gpg.format = "ssh";
+      user.signingkey = "~/.ssh/id_ed25519.pub";
+      user.email = "emma.thorpe@citrix.com";
+    };
   };
   home.packages = [
-	pkgs.kubectl
-	pkgs.tenv
-	pkgs.kubernetes-helm
-	pkgs.azure-cli
-	pkgs.kubelogin
-	pkgs.curl
+    pkgs.kubectl
+    pkgs.argo-rollouts
+	  pkgs.tenv
+	  pkgs.kubernetes-helm
+	  pkgs.azure-cli
+	  pkgs.kubelogin
+    pkgs.curl
+    pkgs.notation
+    pkgs.powershell
+    pkgs.nuget
+    pkgs.gedit
+    pkgs.lens
+    pkgs.python3
+    pkgs.gnumake
+    pkgs.gcc
+    pkgs.libiconv
+    pkgs.autoconf
+    pkgs.automake
+    pkgs.pkg-config
+    pkgs.wget
+    unstable.claude-code
+    pkgs.google-cloud-sdk
   ];
+  home.shellAliases = {
+    docker = "/run/current-system/sw/bin/docker";
+  };
 	programs.tmux = {
 		extraConfig = ''
 			set -g status-right "#(/run/current-system/sw/bin/bash $HOME/code/kube-tmux/kube.tmux 250 red black)"		
 '';
-	};
+  };
+  programs.go = {
+    enable = true;
+  };
 }
